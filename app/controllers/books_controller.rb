76e66b0a1118d01bb226f4ef0b6440a996c263f6
kazuya-hachisuka <35261436+kazuya-hachisuka@users.noreply.github.com>
2018-05-16 13:51:00 +0900

    modified version

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,8 +19,12 @@ class BooksController < ApplicationController
   def create
   	@book = Book.new(book_params)
   	flash[:notice] = "Book was successfully updated."
-  	@book.save
+  	if @book.save
   	redirect_to book_path(@book.id)
+  else
+    @books = Book.all
+    render :index
+  end
   end
 
   def edit
